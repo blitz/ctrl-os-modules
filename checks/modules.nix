1c1040bbbd12b33470ef53e68a4c1d3c4541ac1d
Julian Stecklina <julian.stecklina@cyberus-technology.de>
2026-09-09 14:07:39 +0200

    checks: exclude image module from impurity checks
    The image/repart module has no enable toggle in 26.05. Luckily, it
    doesn't change anything of consequence. The fix has already been
    upstreamed.

--- checks/modules.nix
+++ checks/modules.nix
@@ -4,9 +4,18 @@
 }:
 
 let
+  inherit (pkgs) lib;
   check = import ../lib/check-modules-no-ops.nix {
     inherit pkgs;
-    modules = nixosModules;
+    modules = removeAttrs nixosModules (
+      if lib.versionAtLeast lib.version "26.11" then
+        [ ]
+      else
+        [
+          # See the module for reasons why this is disabled.
+          "image"
+        ]
+    );
   };
 in
 builtins.seq check.result (
